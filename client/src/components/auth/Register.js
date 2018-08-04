ca3905c7abc2efb9e26182ef9f134daca2345a3c
DJ <djxhwill@gmail.com>
2018-08-04 11:55:35 -0700

    Set up Register and Login page

---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,122 @@
+import React, { Component } from "react";
+import axios from "axios";
+import classnames from "classnames";
+
+class Register extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      email: "",
+      password: "",
+      password2: "",
+      errors: {}
+    };
+  }
+
+  handleOnChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleOnSubmit = e => {
+    e.preventDefault();
+    const newUser = {
+      name: this.state.name,
+      email: this.state.email,
+      password: this.state.password,
+      password2: this.state.password2
+    };
+    axios
+      .post("/api/users/register", newUser)
+      .then(res => console.log(res.data))
+      .catch(err => {
+        this.setState({ errors: err.response.data });
+      });
+  };
+
+  render() {
+    const { errors } = this.state;
+    return (
+      <div className="register">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Sign Up</h1>
+              <p className="lead text-center">
+                Create your DevConnector account
+              </p>
+              <form onSubmit={this.handleOnSubmit} noValidate>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className={classnames("form-control form-control-lg", {
+                      "is-invalid": errors.name
+                    })}
+                    placeholder="Name"
+                    name="name"
+                    value={this.state.name}
+                    onChange={this.handleOnChange}
+                  />
+                  {errors.name ? (
+                    <div className="invalid-feedback">{errors.name}</div>
+                  ) : null}
+                </div>
+                <div className="form-group">
+                  <input
+                    type="email"
+                    className={classnames("form-control form-control-lg", {
+                      "is-invalid": errors.email
+                    })}
+                    placeholder="Email Address"
+                    name="email"
+                    value={this.state.email}
+                    onChange={this.handleOnChange}
+                  />
+                  {errors.email ? (
+                    <div className="invalid-feedback">{errors.email}</div>
+                  ) : null}
+                  <small className="form-text text-muted">
+                    This site uses Gravatar so if you want a profile image, use
+                    a Gravatar email
+                  </small>
+                </div>
+                <div className="form-group">
+                  <input
+                    type="password"
+                    className={classnames("form-control form-control-lg", {
+                      "is-invalid": errors.password
+                    })}
+                    placeholder="Password"
+                    name="password"
+                    value={this.state.password}
+                    onChange={this.handleOnChange}
+                  />
+                  {errors.password ? (
+                    <div className="invalid-feedback">{errors.password}</div>
+                  ) : null}
+                </div>
+                <div className="form-group">
+                  <input
+                    type="password"
+                    className={classnames("form-control form-control-lg", {
+                      "is-invalid": errors.password2
+                    })}
+                    placeholder="Confirm Password"
+                    name="password2"
+                    value={this.state.password2}
+                    onChange={this.handleOnChange}
+                  />
+                  {errors.password2 ? (
+                    <div className="invalid-feedback">{errors.password2}</div>
+                  ) : null}
+                </div>
+                <input type="submit" className="btn btn-info btn-block mt-4" />
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default Register;
